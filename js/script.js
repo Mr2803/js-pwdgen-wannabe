6fe9e7f17adc461310d45536ec8a683912a1ca39
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-16 16:23:25 +0200

    show and hidden "devil"

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-var nome = prompt("Qual è il tuo nome?")
-var cognome = prompt("Qual è il tuo cognome?")
-var colore = prompt("Qual è il tuo colore preferito?")
-var saluto = "Ciao " + nome
-var outputfinpassw = nome + cognome + colore + 19
+var nome = prompt("Qual è il tuo nome?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colore = prompt("Qual è il tuo colore preferito?");
+var saluto = "Ciao " + nome;
+var outputFinPassw = nome + cognome + colore + 19;
 
-document.getElementById('presentazione').innerHTML  = saluto + ",questa è la tua password:"
+document.getElementById('presentazione').innerHTML  = saluto + ",questa è la tua password:";
 
-document.getElementById('password').innerHTML =  outputfinpassw
+document.getElementById('password').innerHTML =  outputFinPassw;
 
-document.getElementById('use-me').innerHTML= "Usala per proteggere i tuoi dati bancari"
+document.getElementById('use-me').innerHTML= "Usala per proteggere i tuoi dati bancari";
 
 function devil(){
   document.getElementById("img-devil").style.visibility = "visible";
 
+}
+
+function devil_hidden(){
+  document.getElementById("img-devil").style.visibility = "hidden";
 }

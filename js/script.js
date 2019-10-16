8a5f049fde5832d30d2cf556691d206c821f8d3c
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-16 16:44:16 +0200

    added comments to simplify the code reading

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
+// variabile per il nome
 var nome = prompt("Qual è il tuo nome?");
+
+// variabile per il cognome
 var cognome = prompt("Qual è il tuo cognome?");
+
+// variabile colore preferito
 var colore = prompt("Qual è il tuo colore preferito?");
+
+// variabile per saluto prima della password
 var saluto = "Ciao " + nome;
+
+// variabile per l'output finale password
 var outputFinPassw = nome + cognome + colore + 19;
 
+// output per saluto prima della password
 document.getElementById('presentazione').innerHTML  = saluto + ",questa è la tua password:";
 
+// output finale della password
 document.getElementById('password').innerHTML =  outputFinPassw;
 
+// output "come usare la password?"
 document.getElementById('use-me').innerHTML= "Usala per proteggere i tuoi dati bancari";
 
+// funzioni per far comparire/scomparire devil
 function devil(){
   document.getElementById("img-devil").style.visibility = "visible";
-
 }
 
 function devil_hidden(){

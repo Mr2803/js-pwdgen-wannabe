9028aa65ca0037eb8e1d0a0885cf8884c5281057
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-16 14:58:41 +0200

    try to generate a casual number

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var nome = prompt("Qual è il tuo nome?")
 var cognome = prompt("Qual è il tuo cognome?")
 var colore = prompt("Qual è il tuo colore preferito?")
 var saluto = "Ciao " + nome
+// var numero = Math.round(Math.random()*100);)
 var outputfin = nome + cognome + colore + 19
 
 document.getElementById('password').innerHTML = saluto + ",questa è la tua password:" + outputfin
 
-document.getElementById('use-me').innerHTML= "Usami per proteggere i tuoi dati bancari"
+document.getElementById('use-me').innerHTML= "Usala per proteggere i tuoi dati bancari"

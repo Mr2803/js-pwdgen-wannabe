8b76f08954550b6c833a6d7dda23a289e83bbff6
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-16 13:37:53 +0200

    create basic structure of script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 var nome = prompt("Qual è il tuo nome?")
 var cognome = prompt("Qual è il tuo cognome?")
 var colore = prompt("Qual è il tuo colore preferito?")
+var outputfin = nome + cognome + colore + 19
+
+document.getElementById('password').innerHTML = outputfin

6a96307dc829559c23b41308f7b1ee6b6797b0ad
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-16 15:46:25 +0200

    fix layout

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var nome = prompt("Qual è il tuo nome?")
 var cognome = prompt("Qual è il tuo cognome?")
 var colore = prompt("Qual è il tuo colore preferito?")
 var saluto = "Ciao " + nome
-var outputfin = nome + cognome + colore + 19
+var outputfinpassw = nome + cognome + colore + 19
 
-document.getElementById('presentazione').innerHTML  = saluto + ",questa è la tua password:" 
+document.getElementById('presentazione').innerHTML  = saluto + ",questa è la tua password:"
 
-document.getElementById('password').innerHTML =  outputfin
+document.getElementById('password').innerHTML =  outputfinpassw
 
 document.getElementById('use-me').innerHTML= "Usala per proteggere i tuoi dati bancari"
